0213e2bd0c5af7825a77568a1d7b98fb7f1e8656
Lyud Popov <lyud.popov@gmail.com>
2022-07-16 20:27:31 +0100

    MultiplesOf3Or5

---
+++ Codewars.Katas/MultiplesOf3Or5.cs
@@ -0,0 +1,34 @@
+﻿using NUnit.Framework;
+
+namespace Codewars.Katas
+{
+    public static class MultiplesOf3Or5Kata
+    {
+        public static int Solution(int value)
+        {
+            if (value < 0) return 0;
+
+            int result = 0;
+            
+            for (int i = 1; i < value; i++)
+            {
+                if (i % 3 == 0 || i % 5 == 0)
+                {
+                    result += i;
+                }
+            }
+            
+            return result;
+        }
+    }
+    
+    [TestFixture]
+    public class MultiplesOf3Or5Tests
+    {
+        [Test]
+        public void Test()
+        {
+            Assert.AreEqual(23, MultiplesOf3Or5Kata.Solution(10));
+        }
+    }
+}
